2d133f662c06e4a804d103c95899330e405b03ef
tong <reangsak1234@hotmail.com>
2024-10-21 22:15:44 +0700

    feat: add article section in App-page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { NavBar } from "./components/Nav-bar";
 import { HeroSection } from "./components/HeroSection";
 import { Footer } from "./components/Footer";
+import ArticleSection from "./components/ArticleSection";
 
 function App() {
   return (
     <>
       <NavBar />
       <HeroSection />
+      <ArticleSection />
       <Footer />
     </>
   );
