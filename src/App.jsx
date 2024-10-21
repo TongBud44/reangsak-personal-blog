74a4fb22b0832b11a74b75b067970b8ac4e6f17b
tong <reangsak1234@hotmail.com>
2024-10-21 21:09:45 +0700

    feat: add footer in App-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { NavBar } from "./components/Nav-bar";
 import { HeroSection } from "./components/HeroSection";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <>
       <NavBar />
       <HeroSection />
+      <Footer />
     </>
   );
 }
