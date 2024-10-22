d672aef33d6997084fb8e53a825e3a244f248127
tong <reangsak1234@hotmail.com>
2024-10-22 17:06:40 +0700

    feat: add blog card in article section

--- src/components/ArticleSection.jsx
+++ src/components/ArticleSection.jsx
@@ -1,5 +1,6 @@
 import { Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
+import { blogPosts } from "@/data/blogPosts";
 import {
   Select,
   SelectContent,
@@ -7,60 +8,112 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { BlogCard } from "./BlogCard";
+
 export default function ArticleSection() {
   return (
-    <div className="bg-[#F9F8F6] w-full mx-auto md:px-6 lg:px-[120px] mb-10">
-      <h2 className="text-xl font-bold mb-4 px-4 lg:px-0">Latest articles</h2>
-      <div className="bg-[#EFEEEB] px-4 py-4 md:py-3 md:rounded-sm flex flex-col space-y-4 md:flex-row-reverse md:items-center md:space-y-0 md:justify-between lg:rounded-lg">
-        <div className="w-full md:max-w-sm ">
-          <div className="relative ">
-            <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 lg:text-[#75716B] text-[#75716B]" />
-            <Input
-              type="text"
-              placeholder="Search"
-              className="py-3 lg:rounded-sm placeholder:text-muted-foreground focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:border-muted-foreground lg:placeholder-[#75716B]"
-            />
+    <>
+      <div className="bg-[#F9F8F6] w-full mx-auto md:px-6 lg:px-[120px] mb-10">
+        <h2 className="text-xl font-bold mb-4 px-4 lg:px-0">Latest articles</h2>
+        <div className="bg-[#EFEEEB] px-4 py-4 md:py-3 md:rounded-sm flex flex-col space-y-4 md:flex-row-reverse md:items-center md:space-y-0 md:justify-between lg:rounded-lg">
+          <div className="w-full md:max-w-sm ">
+            <div className="relative ">
+              <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 lg:text-[#75716B] text-[#75716B]" />
+              <Input
+                type="text"
+                placeholder="Search"
+                className="py-3 lg:rounded-sm placeholder:text-muted-foreground focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:border-muted-foreground lg:placeholder-[#75716B]"
+              />
+            </div>
+          </div>
+          <div className="md:hidden w-full">
+            <Select value="highlight">
+              <SelectTrigger className="w-full py-3 rounded-sm text-muted-foreground">
+                <SelectValue placeholder="Select category" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="highlight">Highlight</SelectItem>
+                <SelectItem value="cat">Cat</SelectItem>
+                <SelectItem value="inspiration">Inspiration</SelectItem>
+                <SelectItem value="general">General</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="hidden md:flex space-x-2">
+            <button className="px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium bg-[#DAD6D1] lg:text-[#43403B]">
+              Highlight
+            </button>
+            <button
+              className="px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium bg-[muted] lg:text-[#43403B]
+            "
+            >
+              Cat
+            </button>
+            <button
+              className="px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium bg-[muted] lg:text-[#43403B]
+            "
+            >
+              Inspiration
+            </button>
+            <button
+              className="px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium bg-[muted] lg:text-[#43403B]
+            "
+            >
+              General
+            </button>
           </div>
         </div>
-        <div className="md:hidden w-full">
-          <Select value="highlight">
-            <SelectTrigger className="w-full py-3 rounded-sm text-muted-foreground">
-              <SelectValue placeholder="Select category" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="highlight">Highlight</SelectItem>
-              <SelectItem value="cat">Cat</SelectItem>
-              <SelectItem value="inspiration">Inspiration</SelectItem>
-              <SelectItem value="general">General</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-        <div className="hidden md:flex space-x-2">
-          <button
-            className="px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium bg-[#DAD6D1] lg:text-[#43403B]"
-          >
-            Highlight
-          </button>
-          <button
-            className="px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium bg-[muted] lg:text-[#43403B]
-            "
-          >
-            Cat
-          </button>
-          <button
-            className="px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium bg-[muted] lg:text-[#43403B]
-            "
-          >
-            Inspiration
-          </button>
-          <button
-            className="px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium bg-[muted] lg:text-[#43403B]
-            "
-          >
-            General
-          </button>
-        </div>
       </div>
-    </div>
+      <article className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-0">
+        <BlogCard
+          image={blogPosts[0].image}
+          category={blogPosts[0].category}
+          title={blogPosts[0].title}
+          description={blogPosts[0].description}
+          author={blogPosts[0].author}
+          date={blogPosts[0].date}
+        />
+        <BlogCard
+          image={blogPosts[1].image}
+          category={blogPosts[1].category}
+          title={blogPosts[1].title}
+          description={blogPosts[1].description}
+          author={blogPosts[1].author}
+          date={blogPosts[1].date}
+        />
+        <BlogCard
+          image={blogPosts[2].image}
+          category={blogPosts[2].category}
+          title={blogPosts[2].title}
+          description={blogPosts[2].description}
+          author={blogPosts[2].author}
+          date={blogPosts[2].date}
+        />
+        <BlogCard
+          image={blogPosts[3].image}
+          category={blogPosts[3].category}
+          title={blogPosts[3].title}
+          description={blogPosts[3].description}
+          author={blogPosts[3].author}
+          date={blogPosts[3].date}
+        />
+        <BlogCard
+          image={blogPosts[4].image}
+          category={blogPosts[4].category}
+          title={blogPosts[4].title}
+          description={blogPosts[4].description}
+          author={blogPosts[4].author}
+          date={blogPosts[4].date}
+        />
+        <BlogCard
+          image={blogPosts[5].image}
+          category={blogPosts[5].category}
+          title={blogPosts[5].title}
+          description={blogPosts[5].description}
+          author={blogPosts[5].author}
+          date={blogPosts[5].date}
+        />
+      </article>
+    </>
   );
 }
